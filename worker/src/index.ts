9ab93b7feb4da2e934c8c2818f80d26b6c0427d5
sanjayraj-blip <sanjayraj@campx.in>
2026-04-21 22:42:35 +0530

    Add sortingOrder field to Action model and create migration; initialize package.json, tsconfig.json, and package-lock.json for worker

---
+++ worker/src/index.ts
@@ -0,0 +1,38 @@
+//we need to write the consumer logic of the kafka
+
+import { Kafka } from "kafkajs";
+
+const TOPIC_NAME = "zap-events";
+
+const kafka = new Kafka({
+  clientId: "outbox-processor",
+  brokers: ["localhost:9092"],
+});
+
+async function main() {
+  const consumer = kafka.consumer({ groupId: "main-worker" });
+  await consumer.connect();
+
+  await consumer.subscribe({ topic: TOPIC_NAME, fromBeginning: true });
+
+  await consumer.run({
+    autoCommit: false,
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log({
+        partition,
+        offset: message.offset,
+        value: message.value?.toString(),
+      });
+
+      await consumer.commitOffsets([
+        {
+          topic: TOPIC_NAME,
+          partition: partition,
+          offset: (parseInt(message.offset) + 1).toString(),
+        },
+      ]);
+    },
+  });
+}
+
+main();
